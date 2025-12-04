df711c7bd1fe5ac1e211ac0b4a9b6346e973866f
Adrian Enis <adrianenis2000gmail.com>
2025-12-04 19:18:33 -0400

    commit

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,52 +1,54 @@
-import { Router, type IRouter } from "express"
-import { body } from "express-validator"
-import { createProduct } from "../handlers/product"
-
+import { Router, type IRouter } from "express";
+import { body } from "express-validator";
+import { createProduct } from "../handlers/product";
+import { handleInputErrors } from "../middleware";
 
 //Esto le dice a TypeScript que router es del tipo IRouter, que es el tipo público y portable para routers en Express.
-const router: IRouter = Router()  
+const router: IRouter = Router();
 
 //Routing
 /** router.get, sirve para verificar
  * si nuestro puerto, ademas de escuchar
- * por las conexiones, verifica que el 
+ * por las conexiones, verifica que el
  * servidor este respondiendo a las solicitudes
  * El routing, define que debe responder el servidor
  * segun la ruta
  */
 
+/** GET **/
+router.get("/", (req, res) => {
+  //Cada req - res, tiene sus propios metodos
+  res.json("Desde GET");
+});
 
-       /** GET **/
-router.get("/", (req, res) => {             //Cada req - res, tiene sus propios metodos
-    res.json("Desde GET")
-})
+/** POST **/
+router.post(
+  "/",
+  body("name").notEmpty().withMessage("The product name cannot be empty."),
+  body("price")
+    .isNumeric()
+    .withMessage("invalid value")
+    .notEmpty()
+    .custom((value) => value > 0)
+    .withMessage("Invalid value")
+    .withMessage("The product price cannot be empty."),
+  handleInputErrors,
+  createProduct
+);
 
-        /** POST **/
-router.post("/",
-        body("name")
-            .notEmpty().withMessage("The product name cannot be empty."),
-     
-        body("price")
-        .isNumeric().withMessage("invalid value").notEmpty()
-        .custom(value => value > 0).withMessage("Invalid value")
-        .withMessage("The product price cannot be empty."),
-    
+/** PUT **/
+router.put("/", (req, res) => {
+  res.json("Desde PUT");
+});
 
-    createProduct )
+/** PATCH **/
+router.patch("/", (req, res) => {
+  res.json("Desde PATCH");
+});
 
-        /** PUT **/
-router.put("/", (req, res) => {             
-    res.json("Desde PUT")
-})
+/** DELETE **/
+router.delete("/", (req, res) => {
+  res.json("Desde DELETE");
+});
 
-        /** PATCH **/
-router.patch("/", (req, res) => {             
-    res.json("Desde PATCH")
-})
-
-        /** DELETE **/
-router.delete("/", (req, res) => {          
-    res.json("Desde DELETE")
-})
-
-export default router
+export default router;
--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,22 +1,23 @@
 import { Request, Response } from "express"
-import {check, validationResult} from "express-validator"
+
 import Product from "../models/Product.model"
-import { errorMonitor } from "events"
+
 
 export const createProduct = async(req : Request, res : Response) => {          
     
     
     //Validation
 
-
-    let errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()})
+    try {
+        const product = await Product.create(req.body) // Crea el producto en la BD y espera a que se genere su ID
+         res.json({data: product})  //Returns the created product to the client in JSON format.
+    } catch (error) {
+        console.log(error)
     }
+ 
     
-    const product = await Product.create(req.body) // Crea el producto en la BD y espera a que se genere su ID
 
-    res.json({data: product})  //Returns the created product to the client in JSON format.
+   
                   
     
 }
